7877166a8407074fa5cbe1367f51234be82cf917
Aamir Husnain <134502135+junaid042@users.noreply.github.com>
2024-04-14 11:47:14 +0500

    new

--- commands.js
+++ commands.js
@@ -1,2 +1,2 @@
-Office.onReady((function(){})),Office.actions.associate("action",(function(e){var i={type:Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,message:"Performed action.",icon:"Icon.80x80",persistent:!0};Office.context.mailbox.item.notificationMessages.replaceAsync("ActionPerformanceNotification",i),e.completed()}));
+!function(){function e(e){var i={type:Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,message:"Performed action.",icon:"Icon.80x80",persistent:!0};Office.context.mailbox.item.notificationMessages.replaceAsync("ActionPerformanceNotification",i),e.completed()}Office.onReady((function(){})),Office.actions.associate("Addcategory",e),Office.actions.associate("action",e)}();
 //# sourceMappingURL=commands.js.map
